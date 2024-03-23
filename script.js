0086db6398cc027ef9879cd0d00878650f0a5315
rollienodes <158350450+r0llie@users.noreply.github.com>
2024-03-23 12:24:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let autoPlay = false;
+myVideo.oncanplay = ()=>{
+    setTimeOut(()=>{
+        autoPlay = !myVideo.paused
+    },1000)
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     var video = document.getElementById('myVideo');
     video.play(); // Videoyu otomatik olarak başlat

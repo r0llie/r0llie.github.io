f6982dc5d6efa0d271c8673b0cc81493823029ef
rollienodes <158350450+r0llie@users.noreply.github.com>
2024-03-23 12:12:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('myVideo');
+    video.muted = false; // Videoyu sessiz olmaktan çıkar
+    video.play(); // Videoyu oynat
+});
